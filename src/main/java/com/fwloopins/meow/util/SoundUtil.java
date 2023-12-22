d329cd1af3708032dba7e4accd40dd1171c381ea
Fruitloopins <49851457+Fruitloopins@users.noreply.github.com>
2023-12-22 12:55:57 +1100

    Create new Random instances in SoundUtil

--- src/main/java/com/fwloopins/meow/util/SoundUtil.java
+++ src/main/java/com/fwloopins/meow/util/SoundUtil.java
@@ -6,21 +6,24 @@ import org.bukkit.entity.Player;
 import java.util.Random;
 
 public class SoundUtil {
-    private static final Random random = new Random();
 
     public static void playBegAtPlayer(Player player) {
         player.getWorld().playSound(player.getLocation(), Sound.ENTITY_CAT_BEG_FOR_FOOD, 0.7F, 1.0F);
     }
 
     public static void playHissAtPlayer(Player player) {
+        Random random = new Random();
         player.getWorld().playSound(player.getLocation(), Sound.ENTITY_CAT_HISS, 0.4F, random.nextFloat(0.8F, 1.2F));
     }
 
     public static void playHurtAtPlayer(Player player) {
+        Random random = new Random();
         player.getWorld().playSound(player.getLocation(), Sound.ENTITY_CAT_HURT, 0.65F, random.nextFloat(0.8F, 1.2F));
     }
 
     public static void playMeowAtPlayer(Player player) {
+        Random random = new Random();
+
         Sound sound;
         if (random.nextBoolean()) {
             sound = Sound.ENTITY_CAT_AMBIENT;
@@ -32,10 +35,12 @@ public class SoundUtil {
     }
 
     public static void playPurrAtPlayer(Player player) {
+        Random random = new Random();
         player.getWorld().playSound(player.getLocation(), Sound.ENTITY_CAT_PURR, 0.4F, random.nextFloat(0.8F, 1.2F));
     }
 
     public static void playPurreowAtPlayer(Player player) {
+        Random random = new Random();
         player.getWorld().playSound(player.getLocation(), Sound.ENTITY_CAT_PURREOW, 0.5F, random.nextFloat(0.8F, 1.2F));
     }
 }
