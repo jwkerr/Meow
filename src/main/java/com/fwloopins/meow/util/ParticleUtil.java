e87d4a134ffd74d84f6247ea3efd89f65db7f757
Fruitloopins <49851457+Fruitloopins@users.noreply.github.com>
2023-12-22 12:55:36 +1100

    Position particles correctly

--- src/main/java/com/fwloopins/meow/util/ParticleUtil.java
+++ src/main/java/com/fwloopins/meow/util/ParticleUtil.java
@@ -7,27 +7,28 @@ import org.bukkit.entity.Player;
 import java.util.Random;
 
 public class ParticleUtil {
-    private static final Random random = new Random();
 
     public static void createParticlesAroundPlayer(Player player, Particle particle, int numParticles) {
+        Random random = new Random();
         Location location = player.getLocation();
         double x = location.getX();
         double y = location.getY();
         double z = location.getZ();
 
         for (int i = 0; i < numParticles; i++) {
-            player.getWorld().spawnParticle(particle, random.nextDouble(x, x + 1.0), random.nextDouble(y + 0.2, y + 2), random.nextDouble(z, z + 1.0), 1);
+            player.getWorld().spawnParticle(particle, random.nextDouble(x - 1.0, x + 1.0), random.nextDouble(y + 0.2, y + 2), random.nextDouble(z - 1.0, z + 1.0), 1);
         }
     }
 
     public static void createParticlesAroundPlayerHead(Player player, Particle particle, int numParticles) {
+        Random random = new Random();
         Location location = player.getLocation();
         double x = location.getX();
         double y = location.getY();
         double z = location.getZ();
 
         for (int i = 0; i < numParticles; i++) {
-            player.getWorld().spawnParticle(particle, random.nextDouble(x, x + 1.0), random.nextDouble(y + 1.5, y + 2.2), random.nextDouble(z, z + 1.0), 1);
+            player.getWorld().spawnParticle(particle, random.nextDouble(x - 1.0, x + 1.0), random.nextDouble(y + 1.5, y + 2.2), random.nextDouble(z - 1.0, z + 1.0), 1);
         }
     }
 }
